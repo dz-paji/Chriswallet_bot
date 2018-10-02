dea36a68086f619bec54c803478c20789cfd5900
dz-paji <zhangqian2002@outlook.com>
2018-10-02 22:55:28 +0800

    improvements

--- main.py
+++ main.py
@@ -43,6 +43,10 @@ def pr(bot, update):
     name = update.message.from_user.username
     if name is not None and name.lower() in config.prpr_back:
         text += '我把钱都换成美金了，没钱给你了' + update.message.from_user.username 
+            
+    if config.full_log is True:
+        print('在特殊关照名单中的 '+update.message.from_user.name + ' 发送了 givememoney 指令')
+
 
     update.message.reply_text(text)
 
@@ -50,13 +54,19 @@ def pr(bot, update):
 def pr_other(bot, update):
     update.message.reply_text(config.other_emoticon)
 
+    if config.full_log is True:
+        print(update.message.from_user.name + '发送了 givememoney 指令')
+
+
 def dressing(bot, update):
     dressing_text = ''
     dressing_text += '已经成功为 ' + update.message.from_user.username + ' 穿上女装'
     update.message.reply_text(dressing_text)
+    if config.full_log is True:
+        print(update.message.from_user.name + '发送了 dressing 指令')
 
 def debug(bot, update):
-    print(update.message.text)
+    print(update.message.from_user.name + ' 发送了 ' + update.message.text + '已造成回调失败')
 
 
 def main():
@@ -69,10 +79,11 @@ def main():
     updater.dispatcher.add_handler(MessageHandler(FilterPr(), pr))
     updater.dispatcher.add_handler(MessageHandler(FilterPrOther(), pr_other))
     updater.dispatcher.add_handler(MessageHandler(FilterDressing(), dressing))
-     # updater.dispatcher.add_handler(MessageHandler(Filters.all, debug))
+    updater.dispatcher.add_handler(MessageHandler(Filters.all, debug))
 
     updater.start_polling()
     updater.idle()
+
 
 
 if __name__ == '__main__':

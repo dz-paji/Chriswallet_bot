bb5e6eed96a4c89f3991fdf4e2738c23fe208bc0
dz-paji <zhangqian2002@outlook.com>
2018-10-03 10:30:26 +0800

    improvement

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from telegram.ext import Updater, MessageHandler, BaseFilter, Filters
 import config
 
 
-class FilterPr(BaseFilter):
+class FilterGiveMoneySpecial(BaseFilter):
     def __init__(self):
         self.regex = re.compile(r'/(givememoney)+(@chriswallet(_bot)?)?( |$)')
 
@@ -15,7 +15,7 @@ class FilterPr(BaseFilter):
         return bool(message.text and self.regex.search(message.text) is not None)
 
 
-class FilterPrOther(BaseFilter):
+class FilterGiveMoney(BaseFilter):
     def __init__(self):
         self.regex = re.compile(r'/(givememoney)+(@.+)')
 
@@ -37,49 +37,53 @@ class FilterDressingOther(BaseFilter):
         return bool(message.text and self.regex.search(message.text) is not None)
 
 
-def pr(bot, update):
+def givememoney_list(bot, update):
     text = ''
-
     name = update.message.from_user.username
+
     if name is not None and name.lower() in config.prpr_back:
-        text += '我把钱都换成美金了，没钱给你了' + update.message.from_user.username 
-            
-    if config.full_log is True:
-        print('在特殊关照名单中的 '+update.message.from_user.name + ' 发送了 givememoney 指令')
+        text += '账单已提交，请老板查收 @' + update.message.from_user.username 
+        update.message.reply_text(text)
+    else: 
+        if random.randint(1, 100) <= config.react_rate:
+            text += random.choice(config.react_emoticons)
+            update.message.reply_text(text)
+        else:
+            text += config.default_emoticon
+            update.message.reply_text(config.other_emoticon)
+    
 
-
-    update.message.reply_text(text)
-
-
-def pr_other(bot, update):
+def givememoney(bot, update):
     update.message.reply_text(config.other_emoticon)
-
-    if config.full_log is True:
-        print(update.message.from_user.name + '发送了 givememoney 指令')
 
 
 def dressing(bot, update):
     dressing_text = ''
-    dressing_text += '已经成功为 ' + update.message.from_user.username + ' 穿上女装'
-    update.message.reply_text(dressing_text)
-    if config.full_log is True:
-        print(update.message.from_user.name + '发送了 dressing 指令')
+    name = update.message.from_user.username
+
+    if name is not None and name.lower() in config.dressing_special_list:
+        update.message.reply_text('主人是不可用女装的！')
+    else:
+        dressing_text += '已经成功为 @' + update.message.from_user.username + ' 套上女装'
+        update.message.reply_text(dressing_text)
+
 
 def debug(bot, update):
-    print(update.message.from_user.name + ' 发送了 ' + update.message.text + '已造成回调失败')
+    print(update.message.from_user.name + ' 发送了不存在的 ' + update.message.text + '')
 
 
 def main():
     # logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     config.prpr_back = [name.lower() for name in config.prpr_back]
+    config.dressing_special_list = [name.lower() for name in config.dressing_special_list]
 
     updater = Updater(config.token)
 
-    updater.dispatcher.add_handler(MessageHandler(FilterPr(), pr))
-    updater.dispatcher.add_handler(MessageHandler(FilterPrOther(), pr_other))
+    updater.dispatcher.add_handler(MessageHandler(FilterGiveMoneySpecial(), givememoney_list))
+    updater.dispatcher.add_handler(MessageHandler(FilterGiveMoney(), givememoney))
     updater.dispatcher.add_handler(MessageHandler(FilterDressing(), dressing))
-    updater.dispatcher.add_handler(MessageHandler(Filters.all, debug))
+    updater.dispatcher.add_handler(MessageHandler(Filters.all, debug)) # Debug 
 
     updater.start_polling()
     updater.idle()
